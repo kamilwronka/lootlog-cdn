07e032040c9ac35bd0f81cc9072e05bae651ef70
Kamil Wronka <contact@kamilwronka.com>
2024-03-13 12:30:29 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,33 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const key = url.pathname.slice(1);
+
+    switch (request.method) {
+      case "PUT":
+        await env.LOOTLOG_CDN_BUCKET.put(key, request.body);
+        return new Response(`Put ${key} successfully!`);
+      case "GET":
+        const object = await env.LOOTLOG_CDN_BUCKET.get(key);
+
+        if (object === null) {
+          return new Response("Object Not Found", { status: 404 });
+        }
+
+        const headers = new Headers();
+        object.writeHttpMetadata(headers);
+        headers.set("etag", object.httpEtag);
+
+        return new Response(object.body, {
+          headers,
+        });
+      default:
+        return new Response("Method Not Allowed", {
+          status: 405,
+          headers: {
+            Allow: "GET",
+          },
+        });
+    }
+  },
+};
